ea97e5bd9725da99e38cf56a1e6f12c417ab148e
lolpopwv45 <kazaam2112@gmail.com>
2025-10-01 14:34:51 +0500

    sdelal2

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,10 +2,11 @@
 
 def solve():
 # Ниже пишите решение задачи
-    s =int(input())
+    s = int(input("Введите число:"))
     a = s//2//2
     b = a
-    print(a, s, b)
+    c = s-a-b
+    print(a, c, b)
     
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
