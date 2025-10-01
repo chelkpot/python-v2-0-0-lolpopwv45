1952faf7023af3c4b6397f858840300390e916d0
lolpopwv45 <kazaam2112@gmail.com>
2025-10-01 14:28:37 +0500

    sdelal

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,8 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
-
+    data = input().split()
+    a = int(data[0])
+    b = int(data[1])
+    print(b - 1, a - 1)
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
